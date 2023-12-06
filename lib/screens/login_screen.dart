dc804ec4b8305ff569c8f86cae4a00f4dea4601d
alannadev <alannasp@hotmail.com>
2023-12-06 14:23:08 -0300

    Update login_screen.dart
    Alterando a tela de login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,66 +8,123 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     GlobalKey<FormState> formkey = GlobalKey();
+    FocusNode myFocusNode = new FocusNode();
     String username = "";
     String password = "";
-    return Form(
-      key: formkey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+    return Scaffold(
+        body: Container(
+      alignment: Alignment.center,
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+        image: AssetImage("assets/fundo.png"),
+        fit: BoxFit.cover,
+      )),
+      child: ListView(
         children: [
-          Column(
-            children: [
-              const Text("Insira seu email:",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold)),
-              TextFormField(
-                validator: (String? inValue) {
-                  if (inValue!.isEmpty) {
-                    return "Insira seu email";
-                  }
-                  return null;
-                },
-                onSaved: (String? inValue) {
-                  username = inValue!;
-                },
+          Container(
+            height: 100,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/logo.png"),
+                fit: BoxFit.fitHeight,
               ),
-            ],
+            ),
           ),
-          Column(
-            children: [
-              const Text("Insira a sua senha:",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold)),
-              TextFormField(
-                validator: (String? inValue) {
-                  if (inValue!.length < 4) {
-                    return "Tem que ter ao menos 4 caracters";
-                  }
-                  return null;
-                },
-                onSaved: (String? inValue) {
-                  password = inValue!;
-                },
-              ),
-            ],
-          ),
-          ElevatedButton(
-            onPressed: () {
-              if (formkey.currentState!.validate()) {
-                formkey.currentState!.save();
-                // Lançando evento
-                BlocProvider.of<AuthBloc>(context)
-                    .add(LoginUser(username: username, password: password));
-              }
-            },
-            child: const Text("Logar"),
-          ),
+          Form(
+            key: formkey,
+            child: Column(
+              children: [
+                Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20.0)),
+                          labelText: 'Email',
+                          hintText: 'Digite seu e-mail',
+                          filled: true,
+                          fillColor: Colors.white,
+                          labelStyle: TextStyle(
+                              backgroundColor: myFocusNode.hasFocus
+                                  ? Colors.white
+                                  : Colors.white,
+                              fontWeight: FontWeight.bold,
+                              color: myFocusNode.hasFocus
+                                  ? Colors.white
+                                  : Colors.black),
+                          errorStyle: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              backgroundColor: Colors.red)),
+                      validator: (String? inValue) {
+                        if (inValue!.isEmpty) {
+                          return "Insira seu email";
+                        }
+                        return null;
+                      },
+                      onSaved: (String? inValue) {
+                        username = inValue!;
+                      },
+                    ),
+                    SizedBox(height: 30),
+                  ],
+                ),
+                Column(
+                  children: [
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                                                labelStyle: TextStyle(
+                              backgroundColor: myFocusNode.hasFocus
+                                  ? Colors.white
+                                  : Colors.white,
+                              fontWeight: FontWeight.bold,
+                              color: myFocusNode.hasFocus
+                                  ? Colors.white
+                                  : Colors.black),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20.0)),
+                          labelText: 'Senha',
+                          hintText: 'Digite sua senha',
+                          filled: true,
+                          fillColor: Colors.white,
+                          errorStyle: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              backgroundColor: Colors.red)),
+                      validator: (String? inValue) {
+                        if (inValue!.length < 4) {
+                          return "Tem que ter ao menos 4 caracters";
+                        }
+                        return null;
+                      },
+                      onSaved: (String? inValue) {
+                        password = inValue!;
+                      },
+                    ),
+                    SizedBox(height: 30),
+                  ],
+                ),
+                ElevatedButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.white,
+                  ),
+                  onPressed: () {
+                    if (formkey.currentState!.validate()) {
+                      formkey.currentState!.save();
+                      // Lançando evento
+                      BlocProvider.of<AuthBloc>(context).add(
+                          LoginUser(username: username, password: password));
+                    }
+                  },
+                  child: const Text("Entrar",style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+                ),
+              ],
+            ),
+          )
         ],
       ),
-    );
+    ));
   }
 }
